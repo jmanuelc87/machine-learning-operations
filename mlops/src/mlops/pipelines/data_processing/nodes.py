62f4aa4dad363f7b578dfe7f54e82aeafa3566fb
Juan Manuel Carballo <jm.carb@gmail.com>
2024-10-07 22:22:38 -0600

    Added correlation calculations node

--- mlops/src/mlops/pipelines/data_processing/nodes.py
+++ mlops/src/mlops/pipelines/data_processing/nodes.py
@@ -5,9 +5,23 @@ def convert_to_csv(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-
 def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     renamed = df[['X1','X2','X3','X4','X5','X6','X7','X8','Y1','Y2']] \
-        .rename({'X1':'Compactness','X2':'Surface Area','X3':'Wall Area','X4':'Roof Area','X5':'Height','X6':'Orientation','X7':'Glazing Area','X8':'Glazing Area dist','Y1':'Heating load','Y2':'Cooling load'}, axis=1, inplace=False)
-    
+        .rename({
+            'X1':'Compactness',
+            'X2':'Surface Area',
+            'X3':'Wall Area',
+            'X4':'Roof Area',
+            'X5':'Height',
+            'X6':'Orientation',
+            'X7':'Glazing Area',
+            'X8':'Glazing Area dist',
+            'Y1':'Heating load',
+            'Y2':'Cooling load'}, axis=1, inplace=False)
+
     return renamed
+
+
+def calculate_correlations(df: pd.DataFrame) -> pd.DataFrame:
+    correlations = df.corr(method='pearson')
+    return correlations
--- mlops/src/mlops/pipelines/data_processing/pipeline.py
+++ mlops/src/mlops/pipelines/data_processing/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.8
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import convert_to_csv, rename_columns
+from .nodes import convert_to_csv, rename_columns, calculate_correlations
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -22,7 +22,13 @@ def create_pipeline(**kwargs) -> Pipeline:
         name="rename_columns_node"
     )
     
+    calc_correlations_node = node(
+        func=calculate_correlations,
+        inputs=["csv_renamed_energy_efficiency"],
+        outputs="correlations_energy_efficiency",
+        name="calculate_correlations_node"
+    )
     
     return pipeline(
-        pipe=[to_csv_node, rename_cols_node]
+        pipe=[to_csv_node, rename_cols_node, calc_correlations_node]
     )
