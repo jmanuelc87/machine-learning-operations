151590102357cbe55a9448fa61cf77fa77028e1b
Juan Manuel Carballo <jm.carb@gmail.com>
2024-10-09 19:14:38 -0600

    Added modular pipelines nodes for heating and cooling features

--- mlops/src/mlops/pipelines/data_processing/nodes.py
+++ mlops/src/mlops/pipelines/data_processing/nodes.py
@@ -1,6 +1,6 @@
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sn
+
 
 from sklearn.model_selection import train_test_split
 
@@ -25,24 +25,18 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     return renamed
 
 
-def calculate_stats(df: pd.DataFrame) -> pd.DataFrame:
-    stats = df.describe(include="number").T
-    stats.insert(0, 'Column Names', df.columns)
-
-    return stats
-
-def calculate_correlations(df: pd.DataFrame) -> tuple:
-    correlations = df.corr(method='pearson')
-    plt.figure(figsize=(14,11))
-    sn.heatmap(correlations, annot=True)
-    return correlations, plt
+def remove_null_values(df: pd.DataFrame) -> pd.DataFrame:
+    values_df = df.isnull().sum()
+    all = values_df.to_numpy()
+    if not np.sum(all) > 0:
+        return df
+    else:
+        df = df.dropna()
+        return df
 
 
-def calculate_boxplots(df: pd.DataFrame):
-    df.plot(kind='box', subplots=True, layout=(4,4), sharex=False, sharey=False, figsize=(15,15))
-    return plt
-
-
-def split_train_test_dataset(df: pd.DataFrame, params) -> pd.DataFrame:
-    train_df, test_df = train_test_split(df, test_size=params['test_size'], random_state=params['random_state'], shuffle=True)
-    return train_df, test_df
+def feature_isolation_and_split(df: pd.DataFrame, params):
+    features = df.drop(columns=params['removed'])
+    target = df[params['target']]
+    X_train, X_test, target_train, target_test = train_test_split(features, target,  test_size=params['test_size'], random_state=params['random_state'], shuffle=True)
+    return X_train, X_test, target_train, target_test
--- mlops/src/mlops/pipelines/data_processing/pipeline.py
+++ mlops/src/mlops/pipelines/data_processing/pipeline.py
@@ -1,50 +1,48 @@
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import convert_to_csv, rename_columns, calculate_correlations, calculate_stats, calculate_boxplots, split_train_test_dataset
+from kedro.pipeline.modular_pipeline import pipeline as pipeline_modular
+from .nodes import convert_to_csv, rename_columns, feature_isolation_and_split, remove_null_values
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    to_csv_node = node(
-        func=convert_to_csv,
-        inputs=["raw_energy_efficiency"],
-        outputs="csv_energy_efficiency",
-        name="convert_to_csv_node"
+    base_pipeline = pipeline([
+        node(
+            func=convert_to_csv,
+            inputs=["raw_energy_efficiency"],
+            outputs="csv_energy_efficiency",
+            name="convert_to_csv_node"
+        ),
+        
+        node(
+            func=rename_columns,
+            inputs=["csv_energy_efficiency"],
+            outputs="csv_renamed_energy_efficiency",
+            name="rename_columns_node"
+        ),
+        
+        node(
+            func=remove_null_values,
+            inputs=["csv_energy_efficiency"],
+            outputs="cleaned_csv_energy_efficiency",
+            name="remove_na_node"
+        )
+    ])
+    
+    feature_isolation_and_split_node = node(
+                        func=feature_isolation_and_split,
+                        inputs=["cleaned_csv_energy_efficiency", "params:root"],
+                        outputs=["train_csv_energy_efficiency", "test_csv_energy_efficiency", "train_target_csv_energy_efficiency", "test_target_csv_energy_efficiency"],
+                        name="feature_isolation_and_split_node")
+    
+    heating_pipeline = pipeline_modular(
+        pipe=[feature_isolation_and_split_node],
+        inputs=["cleaned_csv_energy_efficiency"],
+        namespace="heating"
     )
     
-    rename_cols_node = node(
-        func=rename_columns,
-        inputs=["csv_energy_efficiency"],
-        outputs="csv_renamed_energy_efficiency",
-        name="rename_columns_node"
+    cooling_pipeline = pipeline_modular(
+        pipe=[feature_isolation_and_split_node],
+        inputs=["cleaned_csv_energy_efficiency"],
+        namespace="cooling"
     )
     
-    calc_correlations_node = node(
-        func=calculate_correlations,
-        inputs=["csv_renamed_energy_efficiency"],
-        outputs=["correlations_energy_efficiency", "correlations_plot_energy_efficiency"],
-        name="calculate_correlations_node"
-    )
-    
-    calc_stats_node = node(
-        func=calculate_stats,
-        inputs=["csv_renamed_energy_efficiency"],
-        outputs="stats_energy_efficiency",
-        name="calc_stats_node"
-    )
-    
-    calculate_boxplots_node = node(
-        func=calculate_boxplots,
-        inputs=["csv_renamed_energy_efficiency"],
-        outputs="boxplot_energy_efficiency",
-        name="calculate_boxplots_node"
-    )
-    
-    split_train_test_dataset_node = node(
-        func=split_train_test_dataset,
-        inputs=["csv_energy_efficiency", "params:root"],
-        outputs=["train_csv_energy_efficiency", "test_csv_energy_efficiency"],
-        name="split_train_test_dataset_node"
-    )
-    
-    return pipeline(
-        pipe=[to_csv_node, rename_cols_node, calc_correlations_node, calc_stats_node, calculate_boxplots_node, split_train_test_dataset_node]
-    )
+    return base_pipeline + heating_pipeline + cooling_pipeline
