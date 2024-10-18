95d276b4ad0df2039ecaeca0db53cdf20d0224c1
Orlando <orlando.mbaa@gmail.com>
2024-10-18 19:24:44 +0000

    Nodos

--- mlops/src/mlops/pipelines/EDA/nodes.py
+++ mlops/src/mlops/pipelines/EDA/nodes.py
@@ -2,3 +2,16 @@
 This is a boilerplate pipeline 'EDA'
 generated using Kedro 0.19.9
 """
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# 1) Primer nodo, lecture de datos 
+def load_data(filepath: str) -> pd.DataFrame:
+    return pd.read_excel(filepath)
+
+
+
