6946444c7ba1195fe14e644c5077265c8d940839
Orlando <orlando.mbaa@gmail.com>
2024-10-18 02:26:29 +0000

    Agregando la tranformacion del set de datos de testeo (simulando datos en produccion)

---
+++ mlops/src/mlops/pipelines/EDA/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'EDA'
+generated using Kedro 0.19.9
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
