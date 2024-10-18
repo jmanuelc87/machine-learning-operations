de2a7246113c258a7dc9bdbe1dd0766a645056b3
Orlando <orlando.mbaa@gmail.com>
2024-10-18 21:35:40 +0000

    Cleaning pipelines

--- mlops/src/mlops/pipelines/EDA/nodes.py
+++ mlops/src/mlops/pipelines/EDA/nodes.py
@@ -3,15 +3,37 @@ This is a boilerplate pipeline 'EDA'
 generated using Kedro 0.19.9
 """
 
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
 # 1) Primer nodo, lecture de datos 
 def load_data(filepath: str) -> pd.DataFrame:
-    return pd.read_excel(filepath)
+    dataset = pd.read_excel(filepath) 
+    return dataset # Retorno el set de datos leido  
+
+# 2) Rename columns 
+def rename_columns(dataset: pd.DataFrame) -> pd.DataFrame:
+    dataset = dataset.copy()
+    dataset.rename(columns={
+        "X1": "Relative_Compactness",
+        "X2": "Surface_Area",
+        "X3": "Wall_Area",
+        "X4": "Roof_Area",
+        "X5": "Overall_Height",
+        "X6": "Orientation",
+        "X7": "Glazing Area",
+        "X8": "Glazing Area Distribution",
+        "Y1": "Heating Load",
+        "Y2": "Cooling Load"
+    }, inplace=True)
+    return dataset # Retorno un set de datos nenombrado 
 
 
-
+# 3) Get data shape 
+def get_data_shape(dataset: pd.DataFrame) -> pd.DataFrame:
+    shape = {
+        "Cantidad de columnas": [dataset.shape[1]],
+        "Cantidad de filas": [dataset.shape[0]]
+    }
+    return pd.DataFrame(data=shape) # Regresa un pandas DF con el shape del set de datos 
--- mlops/src/mlops/pipelines/EDA/pipeline.py
+++ mlops/src/mlops/pipelines/EDA/pipeline.py
@@ -2,9 +2,50 @@
 This is a boilerplate pipeline 'EDA'
 generated using Kedro 0.19.9
 """
+from kedro.pipeline import Pipeline, node
+from .nodes import load_data, rename_columns, get_data_shape # Importamos las funciones de el archivo .node 
 
-from kedro.pipeline import Pipeline, pipeline
+# 1) Definimos los nodos de forma individual
+
+# 1.1) Definir el nodo de lectura de datos 
+reading_data = node(
+            func= load_data,
+            inputs="params:raw_data_filepath", # La funcion load_data requiere un parámetro, en este caso se definiio en el archivo de paraetres y es un  string donde se define la dirección del archivo 
+            outputs="raw_dataset", # Nombre de su output 
+            name="load_data_node") #nombre del nodo 
 
 
-def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+# 1.2) Definición del nodo de cambio de nombre 
+rename =  node (
+    func=rename_columns,
+    inputs="raw_dataset", # vease que es el output del nodo anterior 
+    outputs="renamed_dataset",
+    name="rename_columns_node"
+)
+
+
+# Kedro se encarga de pasar el dataset al primer parámetro de la función rename_node.
+# 1.3) Definir el nodo boxplot_node
+boxplot = node(
+    func=boxplot_node,  # Función que será llamada en este nodo
+    inputs="renamed_data",  # Utiliza los datos de salida del nodo anterior
+    outputs=None,  # Este nodo no produce un set de datos, simplemente visualiza
+    name="boxplot_node"  # Nombre del nodo para identificarlo en el pipeline
+)
+
+
+# 1.4) Definir el nodo de shape 
+shape = node(
+            func=get_data_shape,
+            inputs="renamed_dataset", # Utiliza el output del nodo anterior 
+            outputs="data_shape",
+            name="get_data_shape_node"
+        )
+
+
+# 2) Integración de nodos
+def create_pipeline(**kwargs):
+    return Pipeline(
+        # Definimos una lista con los nodos del pipeline en orden de ejecución 
+        [ reading_data, rename, boxplotshape]
+    )
