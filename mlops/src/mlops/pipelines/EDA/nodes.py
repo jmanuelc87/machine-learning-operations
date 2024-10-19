dc562cdc1e47f26ad4dd4c40e843042d4e42bb17
Orlando <orlando.mbaa@gmail.com>
2024-10-19 04:41:36 +0000

    Integrando nodos EDA

--- mlops/src/mlops/pipelines/EDA/nodes.py
+++ mlops/src/mlops/pipelines/EDA/nodes.py
@@ -3,6 +3,9 @@ This is a boilerplate pipeline 'EDA'
 generated using Kedro 0.19.9
 """
 
+
+#######################                          ANALISIS EXPLORATORIO DE DATOS                  ###########################
+                        
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
@@ -37,3 +40,58 @@ def get_data_shape(dataset: pd.DataFrame) -> pd.DataFrame:
         "Cantidad de filas": [dataset.shape[0]]
     }
     return pd.DataFrame(data=shape) # Regresa un pandas DF con el shape del set de datos 
+
+# 4) Estadistica descriptiva 
+def descriptive_statistics (dataset: pd.DataFrame) -> pd.DataFrame: 
+    # 4.1) Generamos la estadistica necesaria 
+    dataset = dataset.describe(include="number").T
+    
+    # Meto el indice como una columna 
+    dataset["variable"] = dataset.index 
+    dataset.reset_index(drop = True, inplace =True)
+    
+    # 4.2) Exportamos los resultados 
+    import os
+
+    # Obtención del directorio base, es la dirección base del proyecto 
+    root_path = os.getcwd()
+    
+    # Definir la ruta de exportación, en la ejecución 
+    export_path = root_path + "/data/02_intermediate/EDA"
+    
+    # Comprobar si la carpeta existe, y si no, crearla
+    if not os.path.exists(export_path):
+        os.makedirs(export_path)
+    
+    # Exportación 
+    dataset.to_excel(
+    excel_writer=export_path + "/estadistica_descriptiva.xlsx",  # Path and name of the output file
+    sheet_name='Sheet1',          # Sheet name
+    index=False) 
+
+    return dataset 
+
+
+
+# 5) Generación de histogramas 
+def histograms (dataset:pd.DataFrame): 
+
+    # 5.1) Generación del histogramas
+    dataset.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1, figsize=(12,12))
+
+    # 5.2) Save the figure as PNG
+    import os
+
+    # Obtención del directorio base, es la dirección base del proyecto 
+    root_path = os.getcwd()
+    
+    # Definir la ruta de exportación, en la ejecución 
+    export_path = root_path + "/data/02_intermediate/EDA"
+    
+    # Comprobar si la carpeta existe, y si no, crearla
+    if not os.path.exists(export_path):
+        os.makedirs(export_path)
+    
+    plt.savefig(export_path + "/histograms.png", format="png", dpi=300, bbox_inches='tight')    
+    
+    return plt
--- mlops/src/mlops/pipelines/EDA/pipeline.py
+++ mlops/src/mlops/pipelines/EDA/pipeline.py
@@ -3,49 +3,61 @@ This is a boilerplate pipeline 'EDA'
 generated using Kedro 0.19.9
 """
 from kedro.pipeline import Pipeline, node
-from .nodes import load_data, rename_columns, get_data_shape # Importamos las funciones de el archivo .node 
+from .nodes import load_data, rename_columns, get_data_shape, descriptive_statistics, histograms  # Import all necessary functions
 
-# 1) Definimos los nodos de forma individual
 
-# 1.1) Definir el nodo de lectura de datos 
+#######################             DEFINICION DEL PIPELINE DE EJUCION DE EDA           ###################
+
+# 1) Define individual nodes
+
+# MUY IMPORTANTE: a cada nodo, en la funcion de input le puedes indicar parametros o datos, si le vas a meter parametros que pasaran a la funcion tienen que usar el prefijo params: 
+# 1.1) Data reading node
 reading_data = node(
-            func= load_data,
-            inputs="params:raw_data_filepath", # La funcion load_data requiere un parámetro, en este caso se definiio en el archivo de paraetres y es un  string donde se define la dirección del archivo 
-            outputs="raw_dataset", # Nombre de su output 
-            name="load_data_node") #nombre del nodo 
+    func=load_data,
+    inputs="params:raw_data_filepath",  # Prefix with 'params:', el match ocurre por posicion. Podria ponder una lista de parametros y  se colocarian en el mismo orden a la función que estoy utilizando 
+    outputs="raw_dataset",
+    name="load_data_node")
 
-
-# 1.2) Definición del nodo de cambio de nombre 
-rename =  node (
+# 1.2) Rename node
+rename = node(
     func=rename_columns,
-    inputs="raw_dataset", # vease que es el output del nodo anterior 
+    inputs="raw_dataset",
     outputs="renamed_dataset",
-    name="rename_columns_node"
-)
+    name="rename_columns_node")
 
 
-# Kedro se encarga de pasar el dataset al primer parámetro de la función rename_node.
-# 1.3) Definir el nodo boxplot_node
-boxplot = node(
-    func=boxplot_node,  # Función que será llamada en este nodo
-    inputs="renamed_data",  # Utiliza los datos de salida del nodo anterior
-    outputs=None,  # Este nodo no produce un set de datos, simplemente visualiza
-    name="boxplot_node"  # Nombre del nodo para identificarlo en el pipeline
-)
-
-
-# 1.4) Definir el nodo de shape 
+# 1.3) Shape node
 shape = node(
-            func=get_data_shape,
-            inputs="renamed_dataset", # Utiliza el output del nodo anterior 
-            outputs="data_shape",
-            name="get_data_shape_node"
-        )
+    func=get_data_shape,
+    inputs="renamed_dataset",
+    outputs="data_shape",
+    name="get_data_shape_node")
 
 
-# 2) Integración de nodos
-def create_pipeline(**kwargs):
+# 1.4) Nodo de estadistica descriptiva 
+description = node(
+    func=descriptive_statistics,
+    inputs="renamed_dataset",
+    outputs="descriptive_statistics_excel",  # Reference the catalog output
+    name="descriptive_statistics")
+
+
+# 1.5) Nodo de histogramas 
+histograms  = node(
+    func=histograms,
+    inputs= "renamed_dataset", # Toma un set de datos como input, declarado en el catalogo 
+    outputs= "histograms",  # Reference the catalog output
+    name= "Histograms")
+
+
+# 2) Integrate nodes
+def create_pipeline(**kwargs) -> Pipeline:
     return Pipeline(
-        # Definimos una lista con los nodos del pipeline en orden de ejecución 
-        [ reading_data, rename, boxplotshape]
+        [
+            reading_data,
+            rename,
+            shape,
+            description , 
+            histograms
+        ]
     )
