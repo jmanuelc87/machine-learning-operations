857bc7b6e477116996ab645b213b52a2d90c4f20
Juan Manuel Carballo <jm.carb@gmail.com>
2024-10-22 12:04:08 -0600

    Marshalling model

--- mlops/src/mlops/pipelines/model_development/nodes.py
+++ mlops/src/mlops/pipelines/model_development/nodes.py
@@ -14,36 +14,9 @@ model_dict = {
     "xgboost": XGBRegressor
 }
 
-def _log_mlflow(metric_name, score, model_name, model_params, estimator, input):
-    mlflow.log_params(model_params)
-    mlflow.log_metric(metric_name, score)
-    if model_name != 'xgboost':
-        mlflow.sklearn.log_model(sk_model=estimator, input_example=input, artifact_path=model_name)
-    else:
-        mlflow.xgboost.log_model(xgb_model=estimator, input_example=input, artifact_path=model_name)
-
-
-def train_and_evaluate_model(X: pd.DataFrame, y:pd.DataFrame, model, params) -> pd.DataFrame:
-    SelectedModel = model_dict[model]
-    selected_model = SelectedModel(**params[model])
-    scores = cross_val_score(
-            estimator=selected_model,
-            X=X,
-            y=y.squeeze(),
-            cv=5,
-            scoring='r2',
-            n_jobs=-1
-        )
-
-    return pd.DataFrame(data=scores, columns=[f'r2 {model} scores'])
-
-
-def selection_best_params(X: pd.DataFrame, y: pd.DataFrame, params):
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    mlflow.set_experiment("JuanManuel_Energy_Efficiency")
+def estimate_model(X: pd.DataFrame, y: pd.DataFrame, params, Model):
+    _model = Model()
     
-    SelectedModel = model_dict[params['best_model']]
-    _model = SelectedModel()    
     grid_search = GridSearchCV(
         estimator=_model,
         param_grid=params['best_model_params'],
@@ -53,8 +26,11 @@ def selection_best_params(X: pd.DataFrame, y: pd.DataFrame, params):
         verbose=0
     )
     
-    with mlflow.start_run(tags=params['tags']):
-        grid_search.fit(X, y)
-        _log_mlflow(params['metric'], grid_search.best_score_, params['best_model'], grid_search.best_params_, grid_search.best_estimator_, X)
+    grid_search.fit(X, y)
     
     return _model
+
+
+def estimate_xgb_model(X: pd.DataFrame, y: pd.DataFrame, params):
+    SelectedModel = XGBRegressor
+    return estimate_model(X, y, params, SelectedModel)
